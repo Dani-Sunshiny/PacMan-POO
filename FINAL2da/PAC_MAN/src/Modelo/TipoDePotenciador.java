2be77f36328389d870a347de70c4964a665dee9a
Dani-Sunshiny <danyjkg0916@gmail.com>
2026-06-03 02:18:56 -0500

    Add files via upload

---
+++ FINAL2da/PAC_MAN/src/Modelo/TipoDePotenciador.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Modelo;
+
+/**
+ *
+ * @author User
+ */
+public enum TipoDePotenciador {//aqui iran las palabras claves, para poder saber cuales serán los poderes
+    INVENCIBILIDAD //Implementamos invencibilidad, para que PACMAN no muera 
+    
+}
